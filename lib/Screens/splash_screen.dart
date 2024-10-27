36b2b03db1a618da56107683baa5967320296d09
hayahahmed08 <hayahahmeduser2@gmail.com>
2024-10-27 18:39:28 +0500

    added citie.json

--- lib/Screens/splash_screen.dart
+++ lib/Screens/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weatherapp/Services/weather_services.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -8,68 +9,106 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+  TextEditingController searchController = TextEditingController();
+  List<dynamic> citylist = [];
+  List<dynamic> filteredlist = [];
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Stack(
-            children: [
-              // Background image with decreased opacity
-              Positioned.fill(
-                child: Opacity(
-                  opacity: 1,
-                  child: Image(
-                    image: AssetImage('assets/backgrounds/splashscreenbg.gif'),
-                    fit: BoxFit.cover,
-                  ),
+        body: Stack(
+          children: [
+            Positioned.fill(
+              child: Opacity(
+                opacity: 1,
+                child: Image.asset(
+                  'assets/backgrounds/splashscreenbg.gif',
+                  fit: BoxFit.cover,
                 ),
               ),
-              // Foreground text
-              Center(
-                child: Text.rich(
-                  TextSpan(
-                    text: 'we', // First part of the text
-                    style: TextStyle(
-                      fontSize: 60,
-                      fontFamily: 'Serif',
-                      color: Colors.white,
+            ),
+            Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text.rich(
+                    TextSpan(
+                      text: 'we',
+                      style: TextStyle(
+                        fontSize: 60,
+                        fontFamily: 'Serif',
+                        color: Colors.white,
+                      ),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: '  ther\n',
+                          style: TextStyle(
+                            fontSize: 55,
+                            fontFamily: 'Serif',
+                          ),
+                        ),
+                        TextSpan(
+                          text: ' \t\tA',
+                          style: TextStyle(
+                            fontSize: 90,
+                            fontFamily: 'Serif',
+                            fontWeight: FontWeight.w300,
+                            color: Color(0xFF763645),
+                          ),
+                        ),
+                        TextSpan(
+                          text: 'pp\n',
+                          style: TextStyle(
+                            fontSize: 50,
+                            fontFamily: 'Serif',
+                          ),
+                        ),
+                      ],
                     ),
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: '  ther\n',
-                        style: TextStyle(
-                          fontSize: 55,
-                          fontFamily: 'Serif',
-                        ),
-                      ),
-                      TextSpan(
-                        text: ' \t\tA',
-                        style: TextStyle(
-                          fontSize: 90,
-                          fontFamily: 'Serif',
-                          fontWeight: FontWeight.w300,
-                          color: Color(0xFF763645),
-                        ),
-                      ),
-                      TextSpan(
-                        text: 'pp\n',
-                        style: TextStyle(
-                          fontSize: 50,
-                          fontFamily: 'Serif',
-                        ),
-                      ),
-                    ],
+                    strutStyle: StrutStyle(
+                      fontFamily: 'Serif',
+                      fontSize: 30,
+                      forceStrutHeight: true,
+                    ),
                   ),
-                  strutStyle: StrutStyle(
-                    fontFamily: 'Serif',
-                    fontSize: 30,
-                    forceStrutHeight: true,
+                  SizedBox(height: 20), // Space between text and search box
+                  Container(
+                    width: 300, // Set width as needed
+                    child: TextFormField(
+                      controller: searchController,
+                      decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.symmetric(horizontal: 20),
+                        hintText: 'Search with city ',
+                        hintStyle: TextStyle(color: Colors.white),
+                        filled: true,
+                        fillColor: Colors.transparent,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(50.0),
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(50.0),
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                      ),
+                      style: TextStyle(color: Colors.white),
+                      onChanged: (value) {
+                        setState(() {
+                          filteredlist = citylist
+                              .where((city) => city['city']
+                              .toLowerCase()
+                              .contains(value.toLowerCase()))
+                              .toList();
+                        });
+                      },
+                    ),
                   ),
-                ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,23 @@ import 'package:flutter/scheduler.dart';
 import 'package:weatherapp/Screens/splash_screen.dart';
 
 void main() {
-  runApp(const SplashScreen());
-  debugPrintBeginFrameBanner:false;
+  runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of+-- application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
