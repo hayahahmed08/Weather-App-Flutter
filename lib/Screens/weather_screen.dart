0bcee32eae2de85c75a8f324288612d37d1c13fe
hayahahmed08 <hayahahmeduser2@gmail.com>
2024-10-30 19:26:33 +0500

    Update weather_screen.dart

--- lib/Screens/weather_screen.dart
+++ lib/Screens/weather_screen.dart
@@ -9,6 +9,8 @@ class WeatherScreen extends StatefulWidget {
 class _WeatherScreenState extends State<WeatherScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+    
+    );
   }
 }
